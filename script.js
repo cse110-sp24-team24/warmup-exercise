c7b766e6e199697d35be46d1f1102fbf93983720
HarshGurnani <gurnani.harsh@gmail.com>
2024-04-22 07:20:21 -0700

    initial changes for adding a task item

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const allItems = []; // list of all todo items
 const completed = []; // list of all items that are currently marked as completed
 const uncompleted = []; // list of all items that still need to be completed
+
+function createTodo() {
+  if (
+    document.getElementById("todoInput").value == "" ||
+    document.getElementById("dayInput").value == ""
+  ) {
+    return false;
+  }
+
+  const item = [
+    document.getElementById("todoInput").value,
+    document.getElementById("dayInput").value,
+  ];
+  allItems.push(item);
+  completed.push(item);
+  document.getElementById("todoInput").value = "";
+  document.getElementById("dayInput").value = "";
+  console.log(allItems); // can check that item is being added through Javascript console
+  return false;
+}
