36622ecadba128f7b3d139fb9eb66c1400bee9b0
HarshGurnani <gurnani.harsh@gmail.com>
2024-04-21 17:40:20 -0700

    added initial files and code

---
+++ script.js
@@ -0,0 +1,3 @@
+const allItems = []; // list of all todo items
+const completed = []; // list of all items that are currently marked as completed
+const uncompleted = []; // list of all items that still need to be completed
