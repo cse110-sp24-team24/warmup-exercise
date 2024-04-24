d60e3d935831328a9c7190aaaf03ea25948bafd5
geenalimfat <glimfat@ucsd.edu>
2024-04-24 14:20:39 -0700

    Update s.js
    added done item moves to bottom of the list

--- s.js
+++ s.js
@@ -1,13 +1,25 @@
+// function changeClass(element) {
+//     if (element.classList.contains('todo')) {
+//         element.classList.remove('todo');
+//         element.classList.add('done');
+//     } else {
+//         element.classList.remove('done');
+//         element.classList.add('todo');
+//     }
+// }
 function changeClass(element) {
     if (element.classList.contains('todo')) {
         element.classList.remove('todo');
         element.classList.add('done');
+        
+        // Move the completed item to the bottom of the list
+        const parent = element.parentNode;
+        parent.appendChild(element);
     } else {
         element.classList.remove('done');
         element.classList.add('todo');
     }
 }
-
 
 // This code toggles the class of list items onclick. todo means task is still todo,
 // done means task is done (styling is done in css to represent done vs not done)
@@ -16,7 +28,6 @@ function changeClass(element) {
 
 // TODO
 
-// add functionality that moves list item to bottom of list when it is in "done" class
 
 
 // add a button that removes all "done" class list items from the list
